ecbac8b247d0d9c224d5eaef37fece5e2b7833b5
danileTX <17707221+danileTX@users.noreply.github.com>
2024-10-17 15:28:59 +0800

    feat: farm

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
+
+contract Airdrop {
+    using ECDSA for bytes32;
+    using SafeMath for *;
+
+    IERC20 public airdropToken;
+    uint256 public totalTokensWithdrawn;
+
+    mapping(address => bool) public wasClaimed;
+    uint256 public constant TOKENS_PER_CLAIM = 100 * 10**18;
+
+    event TokensAirdropped(address beneficiary);
+
+    constructor(address _airdropToken) public {
+        require(_airdropToken != address(0));
+
+        airdropToken = IERC20(_airdropToken)
+    }
+
+    function withdrawTokens() public () {
+        require(msg.sender == tx.origin, "require that message sender is tx-origin");
+
+        address beneficiary = msg.sender;
+
+        require(!wasClaimed[beneficiary], "Already claimed!");
+        wasClaimed[msg.sender] = true;
+
+        bool status = airdropToken.transfer(beneficiary, TOKENS_PER_CLAIM);
+        require(status, "Token transfer status is false.");
+
+        totalTokensWithdrawn = totalTokensWithdrawn.add(TOKENS_PER_CLAIM);
+        emit TokensAirdropped(beneficiary);
+    }
+
+}
